09e1435e9a8bedbf3c1f6c1d43eea193e21f28c6
xaliatile <102841177+xaliatile@users.noreply.github.com>
2026-08-10 14:43:45 -0600

    Update Version.lua

--- MaxUI/Version.lua
+++ MaxUI/Version.lua
@@ -1,5 +1,5 @@
 return {
-    TestEnvVersion = "2.0.0",
+    TestEnvVersion = "1.0.4",
     Version = "1.0.0",
-    LibraryBuild = "R" -- PA = Pre-Alpha, A = Alpha, B = Beta, R = Release
+    LibraryBuild = "PA" -- PA = Pre-Alpha, A = Alpha, B = Beta, R = Release
 }
