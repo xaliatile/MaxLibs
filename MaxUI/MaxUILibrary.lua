6e908b095fad88b1d31818bee0e1e6dea0d979aa
xaliatile <102841177+xaliatile@users.noreply.github.com>
2026-08-10 15:21:50 -0600

    Update MaxUILibrary.lua

--- MaxUI/MaxUILibrary.lua
+++ MaxUI/MaxUILibrary.lua
@@ -608,7 +608,7 @@ local function __initUI()
 
     __main.__external.__reactLibrary.AddToJanitor(MainGui)
 
-    for _, AnimatableObject in ipairs(MainGui:GetDescendants())
+    for _, AnimatableObject in ipairs(MainGui:GetDescendants()) do
         if AnimatableObject:IsA("UIObject") and AnimatableObject:GetAttribute("__anim") then
             local RebuiltInfo = TweenInfo.new(
                 AnimatableObject:GetAttribute("__animTime") or 0.5,
